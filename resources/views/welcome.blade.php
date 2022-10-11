fbf6b6db14720f79a188f2ecc355696cab6140fd
mdrazu24 <alif.pab120na.12@gmail.com>
2022-10-11 12:22:51 +0600

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,9 @@
                     </svg>
                 </div>
 
+                <a href="/" >Home</a> |
+                <a href="/contact">Cotact</a>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
